4609252033d2b897dabb6cacfdf6bdf96f212b20
nicolaspc-dev <nicolaspc.wd@gmail.com>
2023-04-14 10:16:01 -0500

    useFavotrite Custom Hook created

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,7 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
-import {BsFillPlayFill} from 'react-icons/bs'
+import {BsFillPlayFill} from 'react-icons/bs';
+import FavoriteButton from './FavoriteButton';
 
 interface MoVieCardProps{
   data: Record<string,any>;
@@ -16,6 +18,7 @@ const MovieCard: React.FC<MoVieCardProps> = ({data}) => {
             <div onClick={() => {}} className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300">
               <BsFillPlayFill size={30}/>
             </div>
+            <FavoriteButton movieId={data?.id}/>
           </div>
           <p className='text-green-400 font-semibold mt-4'>
             New <span className='text-white'>2023</span>
